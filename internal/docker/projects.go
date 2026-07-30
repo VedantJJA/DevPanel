f78d9aefed3de170cc43a79b8eb011a3749ec1f7
Vedant Jadhav <vedantjja@gmail.com>
2026-07-31 03:20:47 +0530

    changed app address

--- internal/docker/projects.go
+++ internal/docker/projects.go
@@ -190,6 +190,16 @@ func HandleDeleteProject(database *db.DB, dockClient *Client) http.HandlerFunc {
 			containerName := fmt.Sprintf("devpnl-%s-%s", projectSlug, svc.Name)
 			_ = dockClient.StopContainer(r.Context(), containerName)
 			_ = dockClient.RemoveContainer(r.Context(), containerName, true)
+
+			// Remove Nginx dynamic subdomain routing
+			subdomain := projectSlug
+			if len(svcs) > 1 && svc.Type != "web" && svc.Type != "static" && svc.Type != "frontend" {
+				subdomain = fmt.Sprintf("%s-%s", projectSlug, svc.Name)
+			}
+			
+			if err := globalNginxManager.RemoveRoute(subdomain); err != nil {
+				log.Printf("nginx_manager: failed to remove route for %s: %v", subdomain, err)
+			}
 		}
 
 		// Purge build logs from memory and disk
